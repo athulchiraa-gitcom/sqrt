6e28a37ebd47c5cc977af6339eba320aaae075f4
athulchiraa-gitcom <athulchira@gmail.com>
2025-11-17 11:35:47 -0800

    Initialize ttimbers-patch-1

---
+++ sqrt.py
@@ -0,0 +1,16 @@
+# author: Tiffany Timbers
+# date: 2020-02-23
+
+"""Calculates and prints the square root of a given number."""
+
+import click
+import math
+
+@click.command()
+@click.option("--n", type=int, required=True, help="Number for which the square root should be calculated")
+def main(n):
+    print(math.sqrt(number))
+    return math.sqrt(number)
+
+if __name__ == "__main__":
+    main()
